8c850f6053800024146f382d58890c8689ade05d
NICHOLE LIN <nlin9675@taehs.org>
2021-02-23 14:48:48 +0000

    Before getting rid of the variables to try to change the css.

---
+++ pt.js
@@ -0,0 +1,35 @@
+function change() {
+  document.getElementById("pop").innerHTML= "Hey Buddy!! How are you?";
+}
+
+function cssChange(){
+  var st1="<link href='style1.css' rel='stylesheet' type='text/css'/>"
+  var st2="<link href='style2.css' rel='stylesheet' type='text/css'/>"
+  var st3="<link href='style3.css' rel='stylesheet' type='text/css'/>"
+  var st4="<link href='style4.css' rel='stylesheet' type='text/css'/>"
+  var st5="<link href='style5.css' rel='stylesheet' type='text/css'/>"
+
+  var style=['st1','st2','st3s','st4','st5']
+
+  document.getElementById("sty").innerHTML=style[0];
+}
+
+function cssChange() {
+  document.getElementById('sty').innerHTML='<link  href="style1.css" rel="stylesheet" type="text/css"/>'
+}
+
+function cssChange0() {
+  document.getElementById('sty').innerHTML='<link  href="style2.css" rel="stylesheet" type="text/css"/>'
+}
+    
+function cssChange1() {
+  document.getElementById('sty').innerHTML='<link  href="style3.css" rel="stylesheet" type="text/css"/>'
+}
+
+function cssChange2() {
+  document.getElementById('sty').innerHTML='<link  href="style4.css" rel="stylesheet" type="text/css"/>'
+}
+
+function cssChange3() {
+  document.getElementById('sty').innerHTML='<link  href="style5.css" rel="stylesheet" type="text/css"/>'
+}
